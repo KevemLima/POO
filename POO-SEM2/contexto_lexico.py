1ec1dd221660820384b68b25b9c4e553c186e177
Kevem Lima <148072437+KevemLima@users.noreply.github.com>
2024-11-19 16:13:51 -0300

    "aula sobre funções - POO"

---
+++ POO-SEM2/contexto_lexico.py
@@ -0,0 +1,12 @@
+#contexto lexico
+
+def calculaMediaIdade(idade1, idade2):
+    idade = (idade1 + idade2) / 2
+    return idade
+
+def calculaSomaIdade(idade1, idade2):
+    idade = idade1 + idade2
+    return idade
+
+print(calculaMediaIdade(10,20))
+print(calculaSomaIdade(10,20))
